238649985e3af0bc82ce38c05dd3b595d0d2ec20
andryushque <andrew.ustinov23@gmail.com>
2020-09-14 19:20:56 +0300

    Create: gallery section + background slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  /*=== Gallery Background Slider ===*/
+  var gallerySwiper = new Swiper(".gallery__items", {
+    loop: true,
+    speed: 1000,
+    autoplay: true,
+  });
+});
